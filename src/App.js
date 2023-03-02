111d0e4a96bfa2f96e48ac6c890df61083342822
Uday Adhikari <49830801+udayadhikari2@users.noreply.github.com>
2023-03-02 16:35:20 +0000

    from codespace

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div className=''>
-     <h1 className="pt-5 h-12 bg-orange font-serif text-center font-bold text-2xl">
+     <h1 className="pt-5 h-12 bg-green font-serif text-center font-bold text-2xl ">
       Hello World
      </h1>
     </div>
