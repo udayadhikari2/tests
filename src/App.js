9d1041aaaea72710dba441bf94a3126715fbb992
Uday Adhikari <uday.adhikari2@gmail.com>
2023-03-02 22:04:58 +0545

    third attempt

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className=''>
+     <h1 className="pt-5 h-12 bg-orange font-serif text-center font-bold text-2xl">
+      Hello World
+     </h1>
+    </div>
+  );
+}
+
+export default App;
