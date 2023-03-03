9a4138f590609b62fd3f79292ac2a2d47fb26885
Uday Adhikari <49830801+udayadhikari2@users.noreply.github.com>
2023-03-03 05:12:00 +0000

    abc

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 
 import * as React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
-import { Card, CardHeader, CardBody, CardFooter,Text} from '@chakra-ui/react'
+import { Card, CardHeader, CardBody,Heading,Stack,StackDivider,Box, Text } from '@chakra-ui/react'
 function App() {
   return (
     <ChakraProvider>
-    <div className=''>
-    <Card>
-  <CardBody>
-    <Text>View a summary of all your customers over the last month.</Text>
-  </CardBody>
-</Card>
-    </div>
-  </ChakraProvider>
-    
+      <div className=''>
+        <Card>
+          <CardHeader>
+            <Heading size='md'>Client Report</Heading>
+          </CardHeader>
+
+          <CardBody>
+            <Stack divider={<StackDivider />} spacing='4'>
+              <Box>
+                <Heading size='xs' textTransform='uppercase'>
+                  Summary
+                </Heading>
+                <Text pt='2' fontSize='sm'>
+                  View a summary of all your clients over the last month.
+                </Text>
+              </Box>
+              <Box>
+                <Heading size='xs' textTransform='uppercase'>
+                  Overview
+                </Heading>
+                <Text pt='2' fontSize='sm'>
+                  Check out the overview of your clients.
+                </Text>
+              </Box>
+              <Box>
+                <Heading size='xs' textTransform='uppercase'>
+                  Analysis
+                </Heading>
+                <Text pt='2' fontSize='sm'>
+                  See a detailed analysis of all your business clients.
+                </Text>
+              </Box>
+            </Stack>
+          </CardBody>
+        </Card>
+      </div>
+    </ChakraProvider>
+
   );
 }
 
